4541c5479754e052b7fbac9e0bb54e0bccc38da8
AbhiGunari <139108097+AbhiGunari@users.noreply.github.com>
2023-09-20 12:23:05 +0530

    Update popup.php

--- popup.php
+++ popup.php
@@ -2,7 +2,7 @@
 $server = "localhost";
 $user = "root";
 $password = "";
-$database = "popup2";
+$database = "popup2.sql";
 $conn = mysqli_connect($server, $user, $password, $database);
 
 if (!$conn) {
